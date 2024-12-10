645f6b147432091af500787512c184501d220fce
Gustaf Johansson <gustajoh@kth.se>
2024-12-10 20:14:09 +0100

    nasty p2 code

--- day_6/part_2.py
+++ day_6/part_2.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 def main():
     matrix = []
     with open('input.txt') as file:
@@ -16,6 +14,7 @@ def startgame(m,i,j,guard):
     current = (i,j)
     guards = ["^",">","v","<"]
     index = guards.index(guard)
+    init_guard = guard
     direction = {
         '^': lambda x, y: (x-1, y),
         '>': lambda x, y: (x, y+1),
@@ -27,23 +26,60 @@ def startgame(m,i,j,guard):
     objs.add(current)
 
     (d_i ,j_i) = direction[guard](*current)
-    distinct = {}
 
     while(checkbounds(m, d_i, j_i)):
         if m[d_i][j_i] == '#':
             index += 1
             index %= 4
             guard = guards[index]
-        else:
-            if current in distinct.keys():
-                if guards[(index+1)%4] == distinct[current] and direction[guard](*current) not in objs:
-                    objs.add(direction[guard](*current))
-            else:
-                distinct[current] = guard
+        else:  
+            res = trywalk(m, (d_i, j_i), (i,j), guard, init_guard, guards, direction)
+            if res[0]:
+                objs.add(res[1])
+
             current = (d_i, j_i)
         (d_i, j_i) = direction[guard](*current)
+
     return len(objs)-1
     
+def trywalk(m, curr, init, guard, init_guard, guards, direction):
+
+    start = init
+    in_vis = {}
+    step = direction[guard](*curr)
+    if not checkbounds(m, step[0], step[1]):
+        return (False, (-1,-1))
+    
+    if m[step[0]][step[1]] != "#":
+        new_obj = step
+    elif m[step[0]][step[1]] == "#":
+        new_obj = direction[guards[(guards.index(guard)+1) % 4]](*curr)
+
+    current = start
+    index = guards.index(init_guard)
+    guard = init_guard
+    (di, dj) = start
+
+    while(checkbounds(m, di, dj)):
+        m[new_obj[0]][new_obj[1]] = "O"
+        if m[di][dj] == '#' or (di, dj) == new_obj:
+            index += 1
+            index %= 4
+            guard = guards[index]
+        else:
+            if (di, dj) not in in_vis.keys():
+                in_vis[(di, dj)] = [guard]
+            elif guard in in_vis[(di, dj)]:
+                return (True, new_obj)
+            else:
+                in_vis[(di, dj)].append(guard)
+
+            
+            current = (di, dj)
+        (di, dj) = direction[guard](*current)
+        
+    return (False, None)
+
 def checkbounds(m,i,j):
     return 0 <= i < len(m) and 0 <= j < len(m[0])
 
