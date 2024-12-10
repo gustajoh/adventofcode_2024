7c3e15e052ae13435339d1d06c159493310708c9
Gustaf Johansson <gustajoh@kth.se>
2024-12-11 00:58:50 +0100

    refactor

--- day_9/part_2.py
+++ day_9/part_2.py
@@ -5,52 +5,32 @@ def main():
 
     b_i = 0
     block_list = []
+    
+    gaps = {}
+    block_size = {}
+
     for i in range(len(drive)):
         if i % 2 == 0:
+            block_size[str(b_i)] = int(drive[i])
             block_list.extend([str(b_i)] * int(drive[i]))
             b_i += 1
         else:
+            gaps[len(block_list)] = int(drive[i])
             block_list.extend("." * int(drive[i]))
 
-    gaps = {}
-    i = 0
-
-    while i < len(block_list):
-        if block_list[i] == ".":
-            check_i = i
-            size = 0
-            while check_i < len(block_list) and block_list[check_i] == ".":
-                size += 1
-                check_i += 1
-            gaps[i] = size
-            if check_i >= len(block_list):
-                break
-            i = check_i
-        else:
-            i += 1
-
-    block_size = {}
     for i in range(len(block_list)-1,-1,-1):
         if block_list[i] != "." and block_list[i] != "0":
             check_i = i
             curr = block_list[check_i]
-            if curr not in block_size.keys():
-                space = 0
-                while check_i > 0 and block_list[check_i] == curr:
-                    space += 1
-                    check_i -= 1
-                check_i += 1
-                block_size[curr] = space
-            
-                gap_i, gap = check_gaps(block_size[curr], gaps)
-                if (gap_i, gap) != (-1, -1) and gap_i < check_i:
-                    for j in range(block_size[curr]):
-                        block_list[i-j], block_list[gap_i+j] = block_list[gap_i+j], block_list[i-j]
+            space = block_size[curr]
+            gap_i, gap = check_gaps(block_size[curr], gaps)
+            if (gap_i, gap) != (-1, -1) and gap_i < check_i:
+                for j in range(block_size[curr]):
+                    block_list[i-j], block_list[gap_i+j] = block_list[gap_i+j], block_list[i-j]
 
-                    del gaps[gap_i]
-                    gaps[gap_i+space] = gap-block_size[curr]
-                    gaps = dict(sorted(gaps.items()))
-
+                del gaps[gap_i]
+                gaps[gap_i+space] = gap-block_size[curr]
+                gaps = dict(sorted(gaps.items()))
 
     checksum = 0
     for i in range(len(block_list)):
